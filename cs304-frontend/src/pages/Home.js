76480d21fd0238e8d6860a569c9083fd09c60f7f
UBC Student <stdntyx@students.cs.ubc.ca>
2024-07-26 23:46:32 -0700

    added react and also a test homepage, routing functionality working as well

---
+++ cs304-frontend/src/pages/Home.js
@@ -0,0 +1,26 @@
+import logo from "../logo.svg";
+import "../App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/pages/Home.js</code> and save to reload.
+        </p>
+        <p>This is the test home screen located at "/".</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
