d239ee81bc693f58f90db70154403820c198df29
UBC Student <stdntyx@students.cs.ubc.ca>
2024-07-31 21:06:52 -0700

    added a screen for looking at requests

--- cs304-frontend/src/App.js
+++ cs304-frontend/src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import Request from "./pages/Request";
+import AdminPage from "./pages/AdminPage";
 import APITest from "./pages/APITest";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/request" element={<Request />} />
+        <Route path="/admin" element={<AdminPage />} />
         <Route path="/testing" element={<APITest />} />
       </Routes>
     </BrowserRouter>
---
+++ cs304-frontend/src/pages/AdminPage.js
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import "./Style.css";
+
+const testList = [
+  {
+    building: "iccs",
+    util: "washroom",
+    desc: "it's a washroom",
+  },
+  {
+    building: "fsc",
+    util: "water fountain",
+    desc: "mmm water",
+  },
+];
+
+function Request(prop) {
+  const request = prop.request;
+  function addRequest() {
+    alert(
+      "Hasn't been implemented yet, suppose I'd need the backend first, also would need a way to generate the details for different utils hmm"
+    );
+    return null;
+  }
+  return (
+    <div className="Request">
+      <p>Building: {request.building}</p>
+      <p> Util type: {request.util} </p>
+      <p> Description: {request.desc} </p>
+      {/* TODO: generate the id */}
+      <button onClick={addRequest}> Accept Request </button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <div className="Navbar">
+        <Link to="/"> Home </Link>
+      </div>
+      <div className="App">
+        <div className="App-header">
+          <h3> Listing Requests </h3>
+          {testList.map((request) => (
+            <Request request={request} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
